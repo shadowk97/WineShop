7fba5bbfe5639306faa42660690b11a5ba04a79c
shadowk97 <shadowk97@gamil.com>
2020-07-09 16:59:26 +0530

    -typos in settings.py and urls.py

--- WineShop/WineShop/settings.py
+++ WineShop/WineShop/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -37,13 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-
     #Own
     'products',
-=======
     'website',
->>>>>>> 28fcd5ff8d8daab1b33a77bfc18c3ac0fdecf235
 ]
 
 MIDDLEWARE = [
--- WineShop/WineShop/urls.py
+++ WineShop/WineShop/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path,include
 from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/',views.view_products,name='products')
+    path('products/',views.view_products,name='products'),
     path('', include('website.urls')),
 ]
