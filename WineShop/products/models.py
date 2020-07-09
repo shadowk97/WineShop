7df4d11a2ea3e5fcf7638a2bdb45244d82510d3e
shadowk97 <shadowk97@gamil.com>
2020-07-09 19:56:04 +0530

    -products (1 bug left first dictionary item)

--- WineShop/products/models.py
+++ WineShop/products/models.py
@@ -5,4 +5,15 @@ class products(models.Model):
     price = models.DecimalField(max_digits=8,decimal_places=2,null=False)
     name = models.TextField(null=False)
     quantity = models.DecimalField(max_digits=4,decimal_places=0)
-    category = models.TextField(null=False)
+    categories = [
+        ('beer', 'Beer'),
+        ('whiskey', 'Whiskey'),
+        ('wine', 'Wine'),
+        ('vodka', 'Vodka'),
+        ('spirits', 'Spirits'),
+    ]
+    category = models.CharField(
+        max_length=7,
+        choices=categories,
+        default='beer',
+    )
