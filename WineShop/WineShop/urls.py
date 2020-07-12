300830b2a5a3693ec053c4fdc4260ab745e7a296
Blank619 <vipulshide0597@gmail.com>
2020-07-12 21:51:08 +0530

    k

--- WineShop/WineShop/urls.py
+++ WineShop/WineShop/urls.py
@@ -22,15 +22,15 @@ urlpatterns = [
     path('home/',views.HomeView,name='home'),
     path('products/',views.view_Products,name='products'),
     path('cart/',views.view_Cart,name='cart'),
-<<<<<<< HEAD
+
     path('address',views.AddressView,name='cart'),
     path('checkout/',views.checkoutView,name='cart'),
     path('checkout/checkout_price/',views.checkout_price),
-=======
+
     path('contact/',views.view_Contact,name='contact'),
     path('address/',views.view_Address,name='address'),
     path('orders/',views.view_Orders,name='orders'),
     path('about/',views.view_About,name='about'),
->>>>>>> 7829a31035937307b62a67e2fb31593ef5fb769f
+
     path('', include('website.urls')),
 ]
